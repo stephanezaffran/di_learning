37f00cb23f599c4ba618a33c5ef7c816f9f8823b
unknown <stephane.zaffran@gmail.com>
2022-01-16 17:37:53 +0200

    exercices xxp

--- week3/day5/ex2.js
+++ week3/day5/ex2.js
@@ -1,45 +1,46 @@
-let language = prompt("what's your language ?");
-language = language.toLowerCase()
-switch (language) {
-    case "hebrew":
-        alert("shalom")
-        break;
-    case "french":
-        alert("salut")
-        break;
-    case "english":
-        alert("hello")
-        break;
-    default:
-        alert("01110011 01101111 01110010 01110010 01111001")
-        break;
-}
+// let language = prompt("what's your language ?");
+// language = language.toLowerCase()
+// switch (language) {
+//     case "hebrew":
+//         alert("shalom")
+//         break;
+//     case "french":
+//         alert("salut")
+//         break;
+//     case "english":
+//         alert("hello")
+//         break;
+//     default:
+//         alert("01110011 01101111 01110010 01110010 01111001")
+//         break;
+// }
 
-//.................ex2................
-let grade = prompt("what's your grade ?");
-grade = parseInt(grade);
-if (grade > 90) {
-    console.log("A")
-} else if (80 < grade <= 90) {
-    console.log("B")
-} else if (70 <= grade <= 80) {
-    console.log("C")
-} else {
-    console.log("D")
-}
+// //.................ex2................
+// let grade = prompt("what's your grade ?");
+// grade = parseInt(grade);
+// if (grade > 90) {
+//     console.log("A")
+// } else if (80 < grade <= 90) {
+//     console.log("B")
+// } else if (70 <= grade <= 80) {
+//     console.log("C")
+// } else {
+//     console.log("D")
+// }
 
 
 
 //.................ex3................
 let verb = prompt("please enter a verb ?")
 verb = verb.toLowerCase();
-if (verb.lenght < 3) {
-    break
+let index = verb.length;
+if (index < 3) {
+
 } else {
-    let index = verb.lenght;
     if (verb[index - 3] == 'i' && verb[index - 2] == 'n' && verb[index - 1] == 'g') {
         verb = verb + "ly";
     } else {
         verb = verb + "ing";
     }
 }
+alert(verb);
