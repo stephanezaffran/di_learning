854cdaa008b33fac2ff4e7a3a123f5d96236063b
unknown <stephane.zaffran@gmail.com>
2022-01-18 21:36:28 +0200

    w4d2

--- week4/Playthegame_MiniProject_W2D5-main/script.js
+++ week4/Playthegame_MiniProject_W2D5-main/script.js
@@ -1,26 +1,35 @@
 function playTheGame() {
-    let play = confirm("do you want to play th game ?")
+    let play = confirm("do you want to play the game ?")
     switch (play) {
         case false:
             alert("No problem, Goodbye")
 
             break;
         case true:
-            let number = prompt("please  enter a number between 0 and 10 ")
-            if (typeof number != 'number') {
-                alert("Sorry Not a number, Goodbye")
+            for (let i = 0; i > 3; i++) {
+                let number = parseInt(prompt("please  enter a number between 0 and 10 "))
+                if (typeof number != 'number') {
+                    alert("Sorry Not a number, Goodbye")
 
-            } else {
-                if (number < 0 && number > 10) {
-                    alert("Sorry it’s not a good number, Goodbye")
                 } else {
-                    let computerNumber = Math.random();
-
-
+                    if (number < 0 && number > 10) {
+                        alert("Sorry it’s not a good number, Goodbye")
+                        break;
+                    } else {
+                        let computerNumber = Math.random();
+                        if (test(computerNumber, number) == "equal") {
+                            alert("winner");
+                        } else if (test(computerNumber, number) == "sup") {
+                            alert("Your number is bigger then the computer's,guess again")
+                        } else if (test(computerNumber, number) == "inf") {
+                            alert("Your number is smaller then the computer's,guess again")
+                        }
+                    }
                 }
-
             }
+            alert("ut of chances")
             break;
+
 
         default:
             break;
@@ -28,7 +37,9 @@ function playTheGame() {
 }
 
 let test = (userNumber, computerNumber) => {
-
+    if (userNumber == computerNumber) { return "equal" }
+    if (userNumber > computerNumber) { return "sup" }
+    if (userNumber < computerNumber) { return "inf" }
 }
 
 // Outside of the playTheGame() function, create a new function named test(userNumber,computerNumber)
@@ -37,8 +48,10 @@ let test = (userNumber, computerNumber) => {
 // The point of this function is to check if the userNumber is the same as the computerNumber:
 // If userNumber is equal to computerNumber, alert “WINNER” and stop the game.
 
-// If userNumber is bigger than computerNumber, alert “Your number is bigger then the computer’s, guess again” (Hint: use the built-in prompt() function to ask the user for a new number).
+// If userNumber is bigger than computerNumber, alert “Your number is bigger then the computer’s,
+// guess again” (Hint: use the built-in prompt() function to ask the user for a new number).
 
-// If userNumber is lower than computerNumber, alert “Your number is smaller then the computer’s, guess again” (Hint: use the built-in prompt() function to ask the user for a new number).
+// If userNumber is lower than computerNumber, alert “Your number is smaller then the computer’s, 
+//guess again” (Hint: use the built-in prompt() function to ask the user for a new number).
 
 // If the user guessed more than 3 times, alert “out of chances” and exit the function.
