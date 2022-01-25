950ac7625e93df41d6c3a5688c3edaa740ba76b9
unknown <stephane.zaffran@gmail.com>
2022-01-25 19:42:21 +0200

    change css javascript

--- week5/day2/ex1.js
+++ week5/day2/ex1.js
@@ -15,7 +15,7 @@
 
 
 // Using a DOM property, retrieve the h1 and console.log it.
-console.log( document.querySelector('h1').textContent)
+console.log(document.querySelector('h1').textContent)
 
 // Using DOM methods, remove the last paragraph in the <article> tag.
 
@@ -25,17 +25,17 @@ doc.removeChild(doc.lastElementChild);
 // Add a event listener which will change the background color of the h2 to red, when it’s clicked on.
 
 let h2 = document.querySelector('h2');
-h2.addEventListener("click",(e) => {e.target.style.backgroundColor = "red"} )
+h2.addEventListener("click", (e) => { e.target.style.backgroundColor = "red" })
 
 // Add an event listener which will hide the h3 when it’s clicked on (use the display:none property).
 
 let h3 = document.querySelector('h3');
-h3.addEventListener("click",(e) => {e.target.style.display = "none"} )
+h3.addEventListener("click", (e) => { e.target.style.display = "none" })
 
 // Add a <button> to the HTML file, that when clicked on, should make the text of all the paragraphs, bold.
 let makeBoldAll = () => {
 
-    [...doc.children].forEach(e => e.style.fontWeight="bold")
+    [...doc.children].forEach(e => e.style.fontWeight = "bold")
 
 }
 
@@ -48,26 +48,26 @@ btn.addEventListener("click", makeBoldAll)
 // BONUS : When you hover on the h1, set the font size to a random pixel size between 0 to 100.(Check out this documentation)
 let sizeOrigin = 0
 
-let randomSize =(e) => {
-   
- let size = Math.floor(Math.random() * 101)
- sizeOrigin = e.target.style.fontSize;
- e.target.style.fontSize = size+"px";
+let randomSize = (e) => {
+
+    let size = Math.floor(Math.random() * 101)
+    sizeOrigin = e.target.style.fontSize;
+    e.target.style.fontSize = size + "px";
 }
 
 
-let resizeOrigine =(e) => {
+let resizeOrigine = (e) => {
 
-    console.log (sizeOrigin)
+    console.log(sizeOrigin)
     e.target.style.fontSize = sizeOrigin;
-   }
+}
 
 
-let h1 =  document.querySelector('h1')
+let h1 = document.querySelector('h1')
 console.log(h1);
 h1.addEventListener("mouseover", randomSize)
-h1.addEventListener("mouseleave", (e) => {  e.target.style.fontSize = "sisizeOriginze";})
-// BONUS : When you hover on the 2nd paragraph, it should fade out (Check out “fade css animation” on Google)
+h1.addEventListener("mouseleave", (e) => { e.target.style.fontSize = "sisizeOriginze"; })
+    // BONUS : When you hover on the 2nd paragraph, it should fade out (Check out “fade css animation” on Google)
 
 
 // Exercise 2 : Work With Forms
@@ -85,7 +85,7 @@ h1.addEventListener("mouseleave", (e) => {  e.target.style.fontSize = "sisizeOri
 
 
 // Retrieve the form and console.log it.
-let form =document.querySelector('form')
+let form = document.querySelector('form')
 console.log(form)
 
 // Retrieve the inputs by their id and console.log them.
@@ -104,10 +104,6 @@ console.log(form.elements['submit'])
 
 
 // When the user submits the form (ie. submit event listener)
-
-
-
-
 // get the values of the input tags,
 // make sure that they are not empty,
 // create an li per input value,
@@ -119,6 +115,16 @@ console.log(form.elements['submit'])
 //     <li>last name of the user</li>
 // </ul>
 
+let subFunction = (e) => {
+
+    console.log([...e.target].forEach(e => {
+        let valueOfInput = e.value;
+        valueOfInput != "" &&
+    }));
+    e.preventDefault();
+}
+
+let mySubmit = form.addEventListener('submit', subFunction)
 
 // Exercise 3 : Transform The Sentence
 // Instructions
