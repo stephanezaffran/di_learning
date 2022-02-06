73ed35eb1fdf8f1e1a8e82e05dd50759466f101c
unknown <stephane.zaffran@gmail.com>
2022-02-06 15:45:21 +0200

    w6d6

--- week6/d6.py
+++ week6/d6.py
@@ -10,7 +10,7 @@
 keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
 my_dic ={}
-for item in zip(keys ,values):
+for item in zip(keys,values):
     my_dic[item[0]] = item[1]
 print(my_dic)
 
@@ -33,16 +33,29 @@ print(my_dic)
 
 family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
 
+total_sum = 0
+
+for x, y in family.items():
+    if y < 3:
+        print(f" for {x} the price is free.")
+    if y >= 3 and y <= 12:
+        print(f" for {x} the price is $10.")
+        total_sum += 10
+    if y > 12:
+        print(f" for {x} the price is $15.")
+        total_sum += 15
+
+
+
 
 #
 # Print out the family’s total cost for the movies.
 # Bonus: Ask the user to input the names and ages instead of using the provided family variable (Hint: ask the user for names and ages and add them into a family dictionary that is initially empty).
 #
+print(f" total to pay for the family {total_sum}.")
 
 
 
-
-#
 # Exercise 3: Zara
 # Instructions
 # Here is some information about a brand.
@@ -59,24 +72,79 @@ family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
 #
 #
 # 2. Create a dictionary called brand which value is the information from part one (turn the info into keys and values).
+brand = {
+    'name': 'Zara',
+    'creation_date': 1975,
+    'creator_name': 'Amancio Ortega Gaona',
+    'type_of_clothes': ['men', 'women', 'children', 'home'],
+    'international_competitors': ['Gap', 'M', 'Benetton'],
+    'number_stores': 7000,
+    'major_color': {
+        'France': 'blue',
+        'Spain': 'red',
+        'US': ['pink', 'green']
+    }
+}
+
 # 3. Change the number of stores to 2.
+brand['number_stores'] = 2
+
 # 4. Print a sentence that explains who Zaras clients are.
+
+
+
 # 5. Add a key called country_creation with a value of Spain.
+
+brand['country_creation'] = "Spain"
+
 # 6. Check if the key international_competitors is in the dictionary. If it is, add the store Desigual.
+
+brand['international_competitors'].append("Desigual")
+
 # 7. Delete the information about the date of creation.
+
+del brand['creation_date']
+
 # 8. Print the last international competitor.
+
+# leng = len(brand['international_competitors'])
+print(brand['international_competitors'][-1])
+
 # 9. Print the major clothes colors in the US.
+
+print(brand['major_color']["US"])
+
 # 10. Print the amount of key value pairs (ie. length of the dictionary).
+
+print(len(brand))
+
 # 11. Print the keys of the dictionary.
+
+for x in brand.items():
+    print(x[0])
+
 # 12. Create another dictionary called more_on_zara with the following details:
 #
 # creation_date: 1975
 # number_stores: 10 000
 #
+more_on_zara = {
+"creation_date": 1975,
+"number_stores": 10000
+}
+
 #
 # 13. Use a method to add the information from the dictionary more_on_zara to the dictionary brand.
+
+for x, y in more_on_zara.items():
+    brand[x] = y
+
 # 14. Print the value of the key number_stores. What just happened ?
-#
+
+
+    print(brand["number_stores"])
+
+# we update the number of stores
 
 
 
@@ -92,16 +160,34 @@ family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
 #
 # #1/
 #
+users = [ "Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+
 # >>> print(disney_users_A)
 # {"Mickey": 0, "Minnie": 1, "Donald": 2, "Ariel": 3, "Pluto": 4}
+
+
+disney_users_A ={}
+
+for index, item in enumerate(users):
+    disney_users_A[item] = index
+print(disney_users_A)
+
+
 #
 # #2/
 #
 # >>> print(disney_users_B)
 # {0: "Mickey",1: "Minnie", 2: "Donald", 3: "Ariel", 4: "Pluto"}
-#
+disney_users_B = {}
+for index, item in enumerate(users):
+    disney_users_B[index] = item
+print(disney_users_B)
+
+
 # #3/
 #
+
+disney_users_c ={}
 # >>> print(disney_users_C)
 # {"Ariel": 0, "Donald": 1, "Mickey": 2, "Minnie": 3, "Pluto": 4}
 #
