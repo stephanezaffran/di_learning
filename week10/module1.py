b5e41eebdfa06fc96865f853b3a879b5e7fc16b6
stephanezaffran <stephane.zaffran@gmail.com>
2022-03-05 21:00:42 +0200

    w10

--- week10/module1.py
+++ week10/module1.py
@@ -1,2 +1,16 @@
-#hello
-#how are you
+# Using the requests and time modules, create a function which returns the amount of time it takes a webpage to load
+# (how long it takes for a complete response to a request).
+# Test your code with multiple sites such as google, ynet, imdb, etc.
+
+import time
+import requests
+
+def request_time(link):
+    t1 = time.time()
+    r = requests.get(link)
+    t2 = time.time() - t1
+    return(time.time() - t1)
+
+print(f" google request time {request_time('https://www.google.fr')}")
+print(f" yahoo request time {request_time('https://www.yahoo.com')}")
+print(f" walla request time {request_time('https://www.walla.co.il')}")
