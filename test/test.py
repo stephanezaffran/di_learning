350caeecadbc56129f2e10dfa25eb336d34205d2
unknown <stephane.zaffran@gmail.com>
2022-03-13 14:43:25 +0200

    w12

--- test/test.py
+++ test/test.py
@@ -1,3 +1,16 @@
 from flask import Flask, render_template
+import faker
 import json
 
+app = Flask(__name__, template_folder="./templates")
+
+
+
+@app.route('/')
+def show():
+
+    return render_template('index.html', title="my title", text="hello to my first app")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
